d4b907c2c462acbf050acf474ae3c51fe3949f9f
admkrl1 <162460553+admkrl1@users.noreply.github.com>
2024-03-20 14:14:26 +0000

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,21 @@ import java.util.Random;
 public class Main {
   public static void main(String[] args) {
     Random generator = new Random();
-    
-    int liczba = generator.nextInt(100);
-    System.out.println(liczba);
 
+    int[] liczby = new int[5];
+
+    int suma=0;
+    
+    System.out.println("Wylosowane liczby:");
+    for(int i=0; i<5; i++){
       
+    liczby[i] = generator.nextInt(100);
+    
+    System.out.println(liczby[i]);
+      suma+=liczby[i];
+    }
+
+    System.out.println("Suma: " + suma);
       
     
   }
