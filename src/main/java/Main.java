6ab42024a8ab348db85171b91f90ec6da38312ed
admkrl1 <162460553+admkrl1@users.noreply.github.com>
2024-03-20 14:05:38 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+public class Main {
+  public static void main(String[] args) {
+    Random generator = new Random();
+    
+    int liczba = generator.nextInt(100);
+    System.out.println(liczba);
+
+      
+      
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
